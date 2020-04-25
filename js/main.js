3244a0b17a31365a512a429ea77ed507204ba41c
SQ_DM <d4417n@yandex.ru>
2020-04-25 14:22:55 +0300

    Добавление ДЗ

---
+++ js/main.js
@@ -0,0 +1,49 @@
+let start = document.getElementById("start");
+console.log(start);
+
+let budgetValue = document.getElementsByClassName("budget-value");
+console.log(budgetValue);
+let optionalExpenses = document.getElementsByClassName(
+  "optionalexpenses-value"
+);
+console.log(optionalExpenses);
+let monthSavings = document.getElementsByClassName("monthsavings-value");
+console.log(monthSavings);
+let yearSavings = document.getElementsByClassName("yearsavings-value");
+console.log(yearSavings);
+
+let input = document.getElementsByClassName("expenses-item");
+console.log(input);
+
+let btn = document.getElementsByTagName("button");
+console.log(btn);
+let expensesItemBtn = btn[0];
+console.log(expensesItemBtn);
+let optionalExpensesBtn = btn[1];
+console.log(optionalExpensesBtn);
+let countBudgetBtn = btn[2];
+console.log(countBudgetBtn);
+
+let optionalExpensesItem = document.querySelectorAll(".optionalexpenses-item");
+console.log(optionalExpensesItem);
+
+let chooseIncome = document.querySelector(".choose-income");
+console.log(chooseIncome);
+
+let checkBox = document.getElementById("savings");
+console.log(checkBox);
+
+let chooseSum = document.querySelector(".choose-sum");
+console.log(chooseSum);
+
+let choosePercent = document.querySelector(".choose-percent");
+console.log(choosePercent);
+
+let yearValue = document.querySelector(".year-value");
+console.log(yearValue);
+
+let month = document.querySelector(".month");
+console.log(month);
+
+let day = document.querySelector(".day");
+console.log(day);
